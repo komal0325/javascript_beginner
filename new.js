616725aa44cd346b73f15797121be28639a5fc58
Hrithik Raj <hrithikraj6400@gmail.com>
2025-07-01 22:10:22 +0530

    video done

--- new.js
+++ new.js
@@ -97,6 +97,26 @@ window.addEventListener("DOMContentLoaded", (e) => {
     })
   })
 
+  const switchbtn = document.querySelector("#switch-btn") 
+  const video = document.querySelector("#video-cont") 
+
+  switchbtn.addEventListener("click", function (){
+    if(!switchbtn.classList.contains("slide")){
+      switchbtn.classList.add("slide")
+      video.pause()
+    }
+    else{
+      switchbtn.classList.remove("slide")
+      video.play()
+    }
+  })
+
+  const preloader = document.querySelector("#preloader")
+
+  window.addEventListener("load", function (){
+    preloader.classList.add("hide-preloader")
+  })
+
 })
 
 function getRandomNumber() {
