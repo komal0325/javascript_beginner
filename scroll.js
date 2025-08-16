464487a927207b1917a182f22b096cfb2f0a6d01
Hrithik Raj <hrithikraj6400@gmail.com>
2025-08-16 23:24:59 +0530

    grocery list completed

--- scroll.js
+++ scroll.js
@@ -232,4 +232,135 @@ window.addEventListener('DOMContentLoaded', (e) => {
     closeBtn.addEventListener('click', closeModal);
     backdrop.addEventListener('click', (e) => { if(e.target === backdrop) closeModal(); });
 
+    // grocery select items
+    const alert = document.querySelector(".alert")
+    const form = document.querySelector(".grocery-form")
+    const grocery = document.querySelector("#grocery")
+    const submitBtn = document.querySelector(".submit-btn")
+    const container = document.querySelector(".grocery-container")
+    const list = document.querySelector(".grocery-list")
+    const clearBtn = document.querySelector(".clear-btn")
+
+    // edit option
+    let editElement
+    let editFlag = false
+    let editID = ""
+
+    // submit forms
+    form.addEventListener('submit', addItem)
+    // clear list
+    clearBtn.addEventListener('click', clearItems)
+    // display items onload
+    window.addEventListener('DOMContentLoaded', setupItems)
+
+
+    // add items
+    function addItem(e) {
+        e.preventDefault()
+        const value = grocery.value
+        const id = new Date().getTime().toString()
+
+        if (value !== "" && !editFlag) {
+            const element = document.createElement("article")
+            let attr = document.createAttribute("data-id")
+            attr.value = id
+            element.setAttributeNode(attr)
+            element.classList.add("grocery-item")
+            element.innerHTML = `<p class="title">${value}</p>
+                        <div class="btn-container">
+                            <button type="button" class="edit-btn">
+                                <i class="fas fa-edit"></i>
+                            </button>
+                            <button type="button" class="delete-btn">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </div>`
+
+            const deleteBtn = element.querySelector(".delete-btn")
+            deleteBtn.addEventListener("click", deleteItem)
+            const editBtn = element.querySelector(".edit-btn")
+            editBtn.addEventListener("click", editItem)
+
+            list.appendChild(element)
+            displayAlert("item added to the list", "success")
+            container.classList.remove("invisible")
+            console.log(container)
+            // addToLocalStorage(id, value)
+            setBackToDefault()
+        }
+        else if (value !== "" && editFlag) {
+            editElement.innerHTML = value
+            displayAlert("value changed", "success")
+            editToLocalStorage(editID, value)
+            setBackToDefault()
+        }
+        else {
+            displayAlert("please enter value", "danger")
+        }
+    }
+    // display alert
+    function displayAlert(text, action) {
+        alert.textContent = text
+        alert.classList.add(`alert-${action}`)
+        // remove alert
+        setTimeout(() => {
+            alert.textContent = ""
+            alert.classList.remove(`alert-${action}`)
+        }, 1500);
+    }
+
+    // clear items
+    function clearItems() {
+        const items = document.querySelectorAll(".grocery-item")
+        if (items.length > 0) {
+            items.forEach(function (item){
+                list.removeChild(item)
+            })
+        }
+        container.classList.add("invisible")
+        console.log(container)
+        displayAlert("empty list", "danger")
+        setBackToDefault()
+        localStorage.removeItem("list")
+    }
+
+    // delete items
+    function deleteItem(e) {
+        const element = e.currentTarget.parentElement.parentElement
+        // const id = element.dataset.id
+
+        list.removeChild(element)
+
+        if (list.children.length === 0) {
+            container.classList.add("invisible")
+            console.log(container)
+        }
+        displayAlert("item removed", "danger")
+
+        setBackToDefault()
+
+        // removeFromLocalStorage(id)
+    }
+
+
+    // edit items
+    function editItem(e) {
+        const element = e.currentTarget.parentElement.parentElement
+
+        editElement = e.currentTarget.parentElement.previousElementSibling
+
+        grocery.value = editElement.innerHTML
+        editFlag = true
+        editID = element.dataset.id
+        submitBtn.textContent = "edit"
+    }
+    function setBackToDefault() {
+        grocery.value = ""
+        editFlag = false
+        editID = ""
+        submitBtn.textContent = "submit"
+    }
+
+
+
 })
